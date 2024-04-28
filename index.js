91f5a810fdbdbe370cde0268ea04110b7af8d5f2
Miguel Ribeiro <miguel.ribeiro.07.contato@gmail.com>
2024-04-28 01:31:39 -0300

    Axios Scraping added, next step take title on h4 anchor

---
+++ index.js
@@ -0,0 +1,63 @@
+const axios = require('axios');
+const {JSDOM} = require('jsdom');
+//const puppeteer = require('puppeteer');
+//const x = require('x-ray')
+
+axios
+    .get("https://logrocket.com/blog")
+    .then(function (response){
+        const dom = new JSDOM(response.data);
+        const titles = [... dom.window.document.querySelectorAll('.post-card-title h4')]
+        if (titles.length > 0){
+            titles.forEach((title)=> console.log(`- ${title}`))
+        }else{
+            console.log('Theres no titles found')
+        }
+    })
+    .catch(function (error){
+        console.error('Error', error)
+    });
+
+//Using Axios to make a HTTPS request in logrocket blog, then Retitle the HTML using regular expressions
+/*axios
+    .get("https://logrocket.com/blog")
+    .then(function (response){
+        const reTitles = /(?<=\<h2 class="card-title"><a\shref=.*?\>).*?(?=\<\/a\>)/g;
+        [... response.data.matchAll(reTitles)].forEach(title => console.log(` - ${title}`));
+    })
+*/
+// Using axios plus JSDOM to take the title elements in logRocket home page
+// '.card-title'means the class and 'a' means the anchor target by querySelectoAll
+
+/*
+const parseLogRocketHome = async () => {
+    try{
+    //Launch the browser and initiate a new page
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    //Navigate to logrocket and wait until connecctions are done
+    await page.goto('https://logrocket.com/blog', {waitUntil:'networkidle2'})
+
+    //Evaluate a function inside the page and returns the result
+    const titles = await page.evaluate(() =>{
+        return [... document.querySelectorAll('.card-title a')].map(el => el.textContent)
+    })
+
+    await browser.close()
+
+    titles.forEach((e) => console.log(`- ${e}`))
+    }catch(error){
+        console.log(error)
+    }
+}
+
+parseLogRocketHome()
+*/
+/*
+x('https://logrocket.com/blog', {
+    titles: ['.card-title a']
+})((err, result) => {
+    result.titles.forEach(title => console.log(`- ${title}`));
+});
+*/
